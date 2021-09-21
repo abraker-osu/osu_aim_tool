3310867deb92c62a3b2aa1d1cafc2d4fef30db6f
abraker <abraker95@gmail.com>
2021-09-21 19:14:14 -0400

    Use QSpinBox instead of QLineEdit

--- app/misc/value_edit.py
+++ app/misc/value_edit.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QSizePolicy, QSlider, QSpacerItem, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import *
 
 
 
@@ -8,40 +8,43 @@ class ValueEdit(QWidget):
     value_changed = pyqtSignal(float)
 
     def __init__(self, minimum, maximum, ticks, name, is_float=False, parent=None):
-        QWidget.__init__(self, parent=parent)
+        QWidget.__init__(self, parent=parent)        
 
         self.verticalLayout = QVBoxLayout(self)
-        self.value_label = QLineEdit(self)
+
+        if is_float:
+            self.value_label = QDoubleSpinBox(self)
+            self.value_label.setDecimals(1)
+        else:
+            self.value_label = QSpinBox(self)
+
         self.verticalLayout.addWidget(self.value_label)
         self.horizontalLayout = QHBoxLayout()
 
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.name_label = QLabel(name)
         self.name_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.verticalLayout.addWidget(self.name_label)
+        self.horizontalLayout.addWidget(self.name_label)
+        
         self.resize(self.sizeHint())
 
-        self.value_label.returnPressed.connect(self.value_enter)
-        self.value_label.textEdited.connect(self.__value_edit)
-        self.value_label.setText(str(0))
+        self.value_label.valueChanged.connect(self.value_enter)
+        self.value_label.textChanged.connect(self.__value_edit)
+        self.value_label.setValue(0)
+        self.value_label.setMaximum(maximum)
+        self.value_label.setMinimum(minimum)
 
-        self.__maximum = maximum
-        self.__minimum = minimum
-
-        self.__is_float = is_float
+        #self.__update_style(self.value_label, 'Draggable', True)
         self.__is_error = False
 
 
     def set_value(self, value):
-        self.value_label.setText(str(value))
+        self.value_label.setValue(value)
         self.value_enter()
 
 
     def get_value(self):
-        if self.__is_float:
-            return float(self.value_label.text())
-        else:
-            return int(float(self.value_label.text()))
+        return self.value_label.value()
 
 
     def is_error(self):
@@ -61,17 +64,5 @@ class ValueEdit(QWidget):
 
 
     def value_enter(self):
-        try:
-            value = self.get_value()
-            self.__update_style(self.value_label, 'Error', False)
-            self.__is_error = False
-        except ValueError:
-            self.__update_style(self.value_label, 'Error', True)
-            self.__is_error = True
-            return
-
         self.__update_style(self.value_label, 'Unsaved', False)
-        
-        value = max(min(self.__maximum, value), self.__minimum)
-        self.value_label.setText(f'{value}')
-        self.value_changed.emit(value)
+        self.value_changed.emit(self.value_label.value())
